9c3ebec66aaa11c57a525cdd10b232d27952f02c
hi <wooluo@126.com>
2025-07-01 02:08:46 +0000

    update

---
+++ tools.sh
@@ -0,0 +1,20 @@
+wget https://github.com/projectdiscovery/subfinder/releases/download/v2.8.0/subfinder_2.8.0_linux_amd64.zip -O subfinder.zip
+unzip -o subfinder.zip
+mv subfinder_linux_amd64 subfinder
+chmod +x subfinder
+
+
+wget https://github.com/projectdiscovery/httpx/releases/download/v1.7.0/httpx_1.7.0_linux_amd64.zip  -O httpx.zip
+unzip -o httpx.zip
+mv httpx_linux_amd64 httpx
+chmod +x httpx
+
+wget https://github.com/projectdiscovery/nuclei/releases/download/v3.4.5/nuclei_3.4.5_linux_amd64.zip -O nuclei.zip
+unzip -o nuclei.zip
+mv nuclei_linux_amd64 nuclei
+chmod +x nuclei
+
+wget https://github.com/projectdiscovery/naabu/releases/download/v2.3.4/naabu_2.3.4_linux_amd64.zip -O naabu.zip
+unzip -o naabu.zip
+mv naabu_linux_amd64 naabu
+chmod +x naabu
